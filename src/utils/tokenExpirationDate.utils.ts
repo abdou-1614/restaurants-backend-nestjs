35bdfdeadeea2d96ce11a76775ed4dd9fcd43821
Abdou <kirakira1614@gmail.com>
2022-11-05 22:14:36 +0100

    Configurate Expiration Date Of RefreshToken

---
+++ src/utils/tokenExpirationDate.utils.ts
@@ -0,0 +1,20 @@
+import ms from "ms"
+import { refreshTokenConfig } from "src/config/jwt.config"
+
+export function getTokenExpirationDate(): Date {
+    const expiresDays = ms(refreshTokenConfig.expiresIn as string) / 1000/ 60 / 60 /24
+
+    const expireAt = addDaysFromNow(expiresDays)
+
+    return expireAt
+}
+
+
+
+function addDaysFromNow(days: number): Date {
+    const result = new Date()
+
+    result.setDate(result.getDate() + days)
+
+    return result
+}
