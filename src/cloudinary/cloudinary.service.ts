efd876378625e55e0951e21c973e5073723ee2e8
Abdou <kirakira1614@gmail.com>
2022-11-05 22:17:39 +0100

    Config JWT

---
+++ src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,21 @@
+import { v2 } from 'cloudinary';
+import { Injectable } from "@nestjs/common";
+import { UploadApiResponse, UploadApiErrorResponse} from 'cloudinary'
+import toStream = require('buffer-to-stream');
+@Injectable()
+export class CloudinaryService {
+    async uploadFile(file: Express.Multer.File, id: string): Promise<UploadApiResponse | UploadApiErrorResponse> {
+        return new Promise((resolve, reject) => {
+            const upload = v2.uploader.upload(file.filename, {public_id: id, folder: 'tmp', format: 'webp' },
+            (error, result) => {
+                if(error) return reject(error)
+                return resolve(result)
+            })
+            toStream(file.buffer).pipe(upload)
+        })
+    }
+
+    async destroyFile(publicId: string) {
+        v2.uploader.destroy(publicId, (error, result) =>result)
+    }
+}
