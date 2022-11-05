44af7791e662334ad3257439246eb44b596135d3
Abdou <kirakira1614@gmail.com>
2022-11-05 19:06:07 +0100

    init file

---
+++ src/constants/auth-action.constants.ts
@@ -0,0 +1,12 @@
+export default {
+    VERIFICATION_CODE_REQUEST: 'verification_code_request',
+    VERIFICATION_CODE_CONFIRMED: 'verification_code_confirmed',
+    VERIFICATION_CODE_REJECTED: 'verification_code_rejected',
+    SIGN_OUT: 'sign_out',
+    SIGN_UP: 'sign_up',
+    SIGN_IN: 'sign_in',
+    FORGOT_PASSWORD: 'forgot_password',
+    RESET_PASSWORD: 'reset_password',
+    REFRESH_ACCESS_TOKEN: 'refresh_access_token',
+    VERIFY_FORGOT_PASSWORD: 'verify_forgot_password',
+}
