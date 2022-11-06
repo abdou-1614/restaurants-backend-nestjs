18a60476950d371c747f379c23a49e1a72e4ca09
Abdou <kirakira1614@gmail.com>
2022-11-06 22:52:30 +0100

    Config NodeMailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    MongooseModule.forRoot(process.env.DB_URI)
+    MongooseModule.forRoot(process.env.DB_URI),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
