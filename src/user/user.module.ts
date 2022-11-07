3fcf6e6d70fc6f3f9024c84ab49b8028f809da6e
Abdou <kirakira1614@gmail.com>
2022-11-07 21:33:08 +0100

    Import Mail Module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,15 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserSchema } from './schema/user.schema';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: 'User', schema: UserSchema}]),
+    MailModule
+],
   controllers: [UserController],
   providers: [UserService]
 })
