df31fb7bb1d2551d4999b83fcffd4f4281abaec5
Abdou <kirakira1614@gmail.com>
2022-11-07 21:33:37 +0100

    Interceptor For File Upload

---
+++ src/common/interceptors/fileUpload.interceptor.ts
@@ -0,0 +1,19 @@
+import { NestInterceptor, Injectable, ExecutionContext, CallHandler } from '@nestjs/common'
+import { Observable } from 'rxjs'
+
+@Injectable()
+export class FileUploadBoduInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const ctx = context.switchToHttp()
+        const req = ctx.getRequest()
+
+        if(req.body && req.file?.fieldname) {
+            const { fieldname } = req.file
+            if(!req.body[fieldname]) {
+                req.body[fieldname] = req.file
+            }
+        }
+
+        return next.handle()
+    }
+}
