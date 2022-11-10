7532f073c0ab870005dc424d7358a0603aacb5a6
Abdou <kirakira1614@gmail.com>
2022-11-10 23:28:07 +0100

    Cloudinary Service Upload

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -4,9 +4,9 @@ import { UploadApiResponse, UploadApiErrorResponse} from 'cloudinary'
 import toStream = require('buffer-to-stream');
 @Injectable()
 export class CloudinaryService {
-    async uploadFile(file: Express.Multer.File, id: string): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    async uploadFile(file: Express.Multer.File): Promise<UploadApiResponse | UploadApiErrorResponse> {
         return new Promise((resolve, reject) => {
-            const upload = v2.uploader.upload(file.filename, {public_id: id, folder: 'tmp', format: 'webp' },
+            const upload = v2.uploader.upload_stream(
             (error, result) => {
                 if(error) return reject(error)
                 return resolve(result)
