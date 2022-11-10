5c8e78c3eb45e642293d959717ba68642d90ed1f
Abdou <kirakira1614@gmail.com>
2022-11-10 23:32:51 +0100

    User Module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,12 +5,15 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserSchema } from './schema/user.schema';
 import { MailModule } from 'src/mail/mail.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { multerUploadConfig } from 'src/utils/multer.utils';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{name: 'User', schema: UserSchema}]),
+    MulterModule.register(multerUploadConfig),
     MailModule,
-    CloudinaryModule
+    CloudinaryModule,
 ],
   controllers: [UserController],
   providers: [UserService]
