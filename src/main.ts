c5595ed9c4be1fb3057cd3b8b6422b9814264951
Abdou <kirakira1614@gmail.com>
2022-11-10 23:27:32 +0100

    Configuration Swagger Api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors()
+
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }))
+
+  const config = new DocumentBuilder()
+  .setTitle('Restaurants Backend')
+  .setDescription('Reastaurants For Backend')
+  .setVersion('0.0.1')
+  .addBearerAuth()
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document, {
+    customSiteTitle: 'Retaurants Swagger API'
+  })
+
   await app.listen(3000);
 }
 bootstrap();
