ec29712924cf51d153489e7def6d3117a8be1b20
Abdou <kirakira1614@gmail.com>
2022-11-12 22:32:27 +0100

    verification handler

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,10 @@
+import { VerifyEmailDto } from './dto/verifiy-email.dto';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseInterceptors } from '@nestjs/common';
 import { UserService } from './user.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileUploadBodyInterceptor } from 'src/common/interceptors/fileUpload.interceptor';
-import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Users')
 @Controller('user')
@@ -11,10 +12,17 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @ApiBody({ type: CreateUserDto })
+  @ApiOperation({ summary: 'Registration User'})
   @ApiConsumes('multipart/form-data')
   @Post()
   @UseInterceptors(FileInterceptor('avatar'), FileUploadBodyInterceptor)
   async create(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto)
+  }
+
+  @ApiOperation({ summary: 'Confirmation User Email'})
+  @Get('verification')
+  async verifiyUserEmail(@Query() {verification}: VerifyEmailDto, id: string) {
+    return this.userService.verifyUser({verification}, id)
   }
 }
