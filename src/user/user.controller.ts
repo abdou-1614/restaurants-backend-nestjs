64984152267409fe0a04d229a5c2f076fcdd0bad
Abdou <kirakira1614@gmail.com>
2022-11-14 22:28:08 +0100

    Update User Handler

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,16 @@
 import { FilterQueryDto } from './dto/find-users.dto';
 import { VerifyEmailDto } from './dto/verifiy-email.dto';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Body, Controller, Get, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseInterceptors, Patch } from '@nestjs/common';
 import { UserService } from './user.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileUploadBodyInterceptor } from 'src/common/interceptors/fileUpload.interceptor';
 import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Param, Req } from '@nestjs/common/decorators';
 import { UserWithoutPassword } from './dto/user-without-password.dto';
+import { UpdateUserInfo } from './dto/update-user.dto';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { User, UserDocument } from './schema/user.schema';
 
 @ApiTags('Users')
 @Controller('user')
@@ -29,14 +32,22 @@ export class UserController {
     return this.userService.verifyUser({verification}, id)
   }
 
-  @Get('/find/:id')
-  async findId(@Param('id') id: string) {
-    // const userId = request.user['userId']
-    return this.userService.findUserById(id)
+  @ApiOperation({ summary: 'Gets user"s own profile' })
+  @Get('/id')
+  async findId(@CurrentUser() userId: UserDocument['_id']) {
+    return this.userService.findUserById(userId)
   }
 
+  @ApiOperation({ summary: 'Getting The Users With Searching And Pagination' })
   @Get()
   async findAllUsers(@Query() query: FilterQueryDto): Promise<UserWithoutPassword[]> {
     return this.userService.findAll(query)
+  }
+
+  @ApiOperation({ summary: 'Update User Profile' })
+  @Patch()
+  async updateUserDetails(@CurrentUser() user: UserDocument, @Body() updateInfo: UpdateUserInfo) {
+    const userId = user._id
+    return this.userService.update(userId, updateInfo)
   }
 }
