dd59fc52fae58c5e82a6f62ee1528fca02f5d502
Abdou <kirakira1614@gmail.com>
2022-11-14 22:25:34 +0100

    Response Without User Password

--- src/user/dto/user-without-password.dto.ts
+++ src/user/dto/user-without-password.dto.ts
@@ -1,4 +1,4 @@
 import { PickType } from "@nestjs/swagger";
 import { UserInteface } from "../interfaces/user.interface";
 
-export class UserWithoutPassword extends PickType(UserInteface, ['fullName', 'email', 'address', 'avatar', 'address',] as const) {}
+export class UserWithoutPassword extends PickType(UserInteface, ['fullName', 'email', 'address', 'avatar', 'address', 'role'] as const) {}
