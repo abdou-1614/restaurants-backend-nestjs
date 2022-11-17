4a9bde053aab9301937e4aad4440ca68ec441402
Abdou <kirakira1614@gmail.com>
2022-11-17 20:30:31 +0100

    JWT Content Data After Decoded

---
+++ src/auth/types/access-token-content.ts
@@ -0,0 +1,13 @@
+export type AccessTokenContent = {
+    /**
+     * User ID 
+     * example: "d6c24523-12df-4f33-9fd6-44dd5c499084"
+     */
+    userId: string,
+
+    /**
+     * User Role
+     * example: "USER"
+     */
+    userRole: string
+}
