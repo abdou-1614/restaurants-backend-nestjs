f82a240f6a2f1327cd8dcb70000f5f3213e952f0
Abdou <kirakira1614@gmail.com>
2022-11-18 21:33:07 +0100

    init file

---
+++ src/auth/dto/logout.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsJWT } from "class-validator";
+
+export class LogoutDto {
+
+    @ApiProperty({
+        example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    })
+    @IsJWT()
+    refreshToken: string
+}
