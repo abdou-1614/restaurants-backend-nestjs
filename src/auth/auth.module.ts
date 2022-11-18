ae7efc8b7ba3579983fcfc30925a768f36d6aea3
Abdou <kirakira1614@gmail.com>
2022-11-18 22:35:11 +0100

    Import MongooseModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,20 @@
+import { RefreshTokenSchema } from './schema/token.schema';
+import { ForgotPasswordSchema } from './schema/forgotPassword.schema';
+import { UserSchema } from './../user/schema/user.schema';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PassportModule, JwtModule.register({})],
+  imports: [
+    MongooseModule.forFeature([ 
+    { name: 'Auth', schema: ForgotPasswordSchema }, 
+    { name: 'Token', schema: RefreshTokenSchema }, 
+    { name: 'User', schema: UserSchema } ]), 
+    PassportModule, JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService]
 })
