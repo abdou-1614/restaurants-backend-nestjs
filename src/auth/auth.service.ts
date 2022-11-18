44213a875fb0e3766d3d82658eac427b2723c406
Abdou <kirakira1614@gmail.com>
2022-11-18 22:36:11 +0100

    Import Inject Model

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
+import { RefreshTokenDocument } from './schema/token.schema';
+import { UserDocument } from './../user/schema/user.schema';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ForgotPasswordDocument } from './schema/forgotPassword.schema';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor( 
+        @InjectModel('User') private userModel: Model<UserDocument>,
+        @InjectModel('Token') private refreshTokenModel: Model<RefreshTokenDocument>,
+        @InjectModel('Auth') private forgotPasswordModel: Model<ForgotPasswordDocument>
+        ) {}
+}
