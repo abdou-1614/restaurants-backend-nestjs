bf493675ca43f50dc426df9eafa42e7deb634959
Abdou <kirakira1614@gmail.com>
2022-11-30 21:46:00 +0100

    RefreshToken Handler

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
+import { RefreshTokenDto } from './dto/refreshToken.dto';
 import { AuthLoginDto } from './dto/auth-login.dto';
 import { Body, Controller, HttpCode, HttpStatus, Post, Req } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { Public } from './public.decorator';
 import { Request } from 'express';
@@ -13,11 +14,21 @@ export class AuthController {
 
   @ApiOperation({ summary: 'User Log In' })
   @Public()
-  @Post()
+  @Post('/login')
   @HttpCode(HttpStatus.OK)
   async login(@Req() request: Request, @Body() { email, password }: AuthLoginDto): Promise<AuthLoginResponse> {
       const browserInfo = `${request.ip} ${request.headers['user-agent']} ${request.headers['accept-language']}`.replace(/ undefined/g, '')
 
       return this.authService.signIn(email, password, browserInfo)
+  }
+
+  @ApiOkResponse({ type: AuthLoginResponse })
+  @ApiOperation({ summary: 'RefereshToken' })
+  @Public()
+  @Post('/refresToken')
+  async refreshToken(@Req() request: Request, @Body() { refreshToken }: RefreshTokenDto): Promise<AuthLoginResponse> {
+    const browserInfo = `${request.ip} ${request.headers['user-agent']} ${request.headers['accept-language']}`.replace(/ undefined/g, '')
+
+    return this.authService.refreshToken(refreshToken, browserInfo)
   }
 }
