c3a28e690eb2b76556c397c45586de5a333e3e27
Abdou <kirakira1614@gmail.com>
2022-11-30 20:45:45 +0100

    Import APP_GUARD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessJwtGuard } from './auth/auth-jwt.guard';
 
 @Module({
   imports: [
@@ -15,7 +15,11 @@ import { AuthModule } from './auth/auth.module';
     UserModule,
     AuthModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AccessJwtGuard
+    }
+  ],
 })
 export class AppModule {}
