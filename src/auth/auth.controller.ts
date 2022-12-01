b88a250c248060a9370dfee9e4c058b599bd86b3
Abdou <kirakira1614@gmail.com>
2022-12-01 14:12:31 +0100

    Get All Active Users

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
 import { RefreshTokenDto } from './dto/refreshToken.dto';
 import { AuthLoginDto } from './dto/auth-login.dto';
-import { Body, Controller, HttpCode, HttpStatus, Post, Req } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req } from '@nestjs/common';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { Public } from './public.decorator';
 import { Request } from 'express';
 import { AuthLoginResponse } from './dto/auth-login-response.dto';
+import { LogoutDto } from './dto/logout.dto';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { UserDocument } from 'src/user/schema/user.schema';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -30,5 +33,26 @@ export class AuthController {
     const browserInfo = `${request.ip} ${request.headers['user-agent']} ${request.headers['accept-language']}`.replace(/ undefined/g, '')
 
     return this.authService.refreshToken(refreshToken, browserInfo)
+  }
+
+  @ApiOperation({ summary: 'Log Out User' })
+  @ApiBearerAuth()
+  @Post('/logout')
+  async logout(@Body() { refreshToken }: LogoutDto) {
+    return this.authService.logout(refreshToken)
+  }
+
+  @ApiOperation({ summary: 'Logs out user of all sessions' })
+  @ApiBearerAuth()
+  @Post('/logoutAll')
+  async logouAll(@CurrentUser() userId: UserDocument['_id']) {
+    return this.authService.logoutAll(userId)
+  }
+
+  @ApiOperation({ summary: 'Returns all user active tokens' })
+  @ApiBearerAuth()
+  @Get('/tokens')
+  async findAllTokens(@CurrentUser() userId: UserDocument['_id']){
+    return this.authService.findAllTokens(userId)
   }
 }
