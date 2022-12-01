d054d8fd07adf029060a24581a981e0ba7937326
Abdou <kirakira1614@gmail.com>
2022-12-01 15:06:18 +0100

    change email to IsOptional

--- src/auth/dto/verify-forgot-password.dto.ts
+++ src/auth/dto/verify-forgot-password.dto.ts
@@ -1,8 +1,22 @@
 import { AuthLoginDto } from './auth-login.dto';
 import { ApiProperty, PickType } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber, IsOptional, MaxLength, MinLength } from 'class-validator';
 
-export class VerifyForgotPasswordDto extends PickType(AuthLoginDto, ['email'] as const) {
+export class VerifyForgotPasswordDto {
+
+    @ApiProperty({
+        type: String,
+        example: 'user@gmail.com',
+        description: 'the unique email for each user',
+        uniqueItems: true,
+        minLength: 5,
+        maxLength: 256,
+      })
+    @IsOptional()
+    @MinLength(5)
+    @MaxLength(256)
+    @IsEmail()
+    readonly email?: string;
 
     @ApiProperty({
         example: '123456',
