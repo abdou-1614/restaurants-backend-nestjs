83ffdab0cf89263a57b44ac2d3a89039c27d54dd
Abdou <kirakira1614@gmail.com>
2022-12-04 22:16:56 +0100

    init file

---
+++ src/restaurant/schema/location.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+
+@Schema()
+export class Location {
+
+    @Prop({ type: String,  enum: ['Point']})
+    type: string
+
+    @Prop({ index: '2dsphere' })
+    coordinates: Number[]
+
+
+    formattedAddress: string;
+    city: string;
+    state: string;
+    zipcode: string;
+    country: string;
+}
