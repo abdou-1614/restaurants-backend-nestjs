fec1ecadc26e213a0f99fb422f351b7818d6ad4b
Abdou <kirakira1614@gmail.com>
2022-12-08 22:54:40 +0100

    Interceptor For Multi file

--- src/common/interceptors/multi-file.interceptor.ts
+++ src/common/interceptors/multi-file.interceptor.ts
@@ -6,8 +6,8 @@ export class MultiFilesBodyInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
         const request = context.switchToHttp().getRequest()
 
-        if(request.body && Array.isArray(request.file) && request.file) {
-            request.file.forEach((file: Express.Multer.File) => {
+        if(request.body && Array.isArray(request.files) && request.files.length) {
+            request.files.forEach((file: Express.Multer.File) => {
                 const { fieldname } = file
                 if(!request.body[fieldname]) {
                     request.body[fieldname] = [file]
