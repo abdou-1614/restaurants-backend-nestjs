98f9a9731e903c39c6945d3949c3158562f398a6
Abdou <kirakira1614@gmail.com>
2022-12-08 22:55:09 +0100

    Get UserId from Request User

--- src/restaurant/restaurant.controller.ts
+++ src/restaurant/restaurant.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { MultiFilesBodyInterceptor } from './../common/interceptors/multi-file.interceptor';
+import { Body, Controller, Post, Req, UseInterceptors } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation } from '@nestjs/swagger';
+import { Request } from 'express';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { RestaurantService } from './restaurant.service';
+import { Public } from 'src/auth/public.decorator';
 
 @Controller('restaurant')
 export class RestaurantController {
   constructor(private readonly restaurantService: RestaurantService) {}
+
+  @ApiBody({ type: CreateRestaurantDto })
+  @ApiConsumes('multipart/form-data')
+  @ApiOperation({ summary: 'Create Restaurant' })
+  @ApiBearerAuth()
+  @Post('create-restaurant')
+  @UseInterceptors(FilesInterceptor('images'), MultiFilesBodyInterceptor)
+  async create(@Body() createInput: CreateRestaurantDto, @Req() request: Request) {
+    const userId = request.user['userId'] 
+    return this.restaurantService.createRestaurant(createInput, userId)
+  }
 }
