09b5f1e8aa4d15dd5f37f2bf48c69300a3b08c60
Abdou <kirakira1614@gmail.com>
2022-12-08 22:55:31 +0100

    Import Multer Module

--- src/restaurant/restaurant.module.ts
+++ src/restaurant/restaurant.module.ts
@@ -1,8 +1,18 @@
+import { CloudinaryModule } from './../cloudinary/cloudinary.module';
 import { Module } from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 import { RestaurantController } from './restaurant.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RestaurantSchema } from './schema/restaurant.schema';
+import { MulterModule } from '@nestjs/platform-express';
+import { multerUploadConfig } from 'src/utils/multer.utils';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Restaurant', schema: RestaurantSchema }]),
+    MulterModule.register(multerUploadConfig),
+    CloudinaryModule,
+  ],
   controllers: [RestaurantController],
   providers: [RestaurantService]
 })
