d1474eb4db00b345d5ba4b7b477755c978554d05
Abdou <kirakira1614@gmail.com>
2022-12-10 22:51:31 +0100

    add ApiBearerAuth To Route

--- src/restaurant/restaurant.controller.ts
+++ src/restaurant/restaurant.controller.ts
@@ -1,12 +1,13 @@
 import { MultiFilesBodyInterceptor } from './../common/interceptors/multi-file.interceptor';
-import { Body, Controller, Post, Req, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req, UseInterceptors } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
-import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { RestaurantService } from './restaurant.service';
-import { Public } from 'src/auth/public.decorator';
+import { FindRestaurantQueryDto } from './dto/find-restaurant.dto';
 
+@ApiTags('Restaurant')
 @Controller('restaurant')
 export class RestaurantController {
   constructor(private readonly restaurantService: RestaurantService) {}
@@ -20,5 +21,12 @@ export class RestaurantController {
   async create(@Body() createInput: CreateRestaurantDto, @Req() request: Request) {
     const userId = request.user['userId'] 
     return this.restaurantService.createRestaurant(createInput, userId)
+  }
+
+  @ApiOperation({ summary: 'Find Restaurant' })
+  @ApiBearerAuth()
+  @Get('get-restaurant')
+  async find(@Query() query: FindRestaurantQueryDto) {
+    return this.restaurantService.findAll(query)
   }
 }
