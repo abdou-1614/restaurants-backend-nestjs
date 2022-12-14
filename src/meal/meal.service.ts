4c5ca480463b0b5c5e4c404afd6e6462cd5b08bf
Abdou <kirakira1614@gmail.com>
2022-12-14 22:26:00 +0100

    Init File

--- src/meal/meal.service.ts
+++ src/meal/meal.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Restaurant } from 'src/restaurant/schema/restaurant.schema';
+import { Meal } from './schema/meal.schema';
 
 @Injectable()
-export class MealService {}
+export class MealService {
+    constructor(
+        @InjectModel(Restaurant.name) private restaurantModel: Model<Restaurant>,
+        @InjectModel(Meal.name) private mealModel: Model<Meal>
+        ) {}
+}
