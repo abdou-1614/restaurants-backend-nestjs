3e593d30b99178e3b955d173b89b8d92d4a986e9
Abdou <kirakira1614@gmail.com>
2022-12-14 22:28:32 +0100

    Import Meal Schema

--- src/meal/meal.module.ts
+++ src/meal/meal.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MealService } from './meal.service';
 import { MealController } from './meal.controller';
+import { RestaurantSchema } from 'src/restaurant/schema/restaurant.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MealSchema } from './schema/meal.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Restaurant', schema: RestaurantSchema }, { name: 'Meal', schema: MealSchema }]),
+  ],
   controllers: [MealController],
   providers: [MealService]
 })
