d7cdba5f78691ddffe759fd52be874426b148030
Abdou <kirakira1614@gmail.com>
2022-12-14 22:48:27 +0100

    Create Meal

--- src/meal/meal.controller.ts
+++ src/meal/meal.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { CreateMealDto } from './dto/create-meal.dto';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { MealService } from './meal.service';
+import { Request } from 'express';
+import { ApiBearerAuth, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Meal')
 @Controller('meal')
 export class MealController {
   constructor(private readonly mealService: MealService) {}
+
+  @ApiCreatedResponse({
+    description: 'Meal Created SuccessFul',
+    type: CreateMealDto
+  })
+  @ApiOperation({ summary: 'Create Meal' })
+  @ApiBearerAuth()
+  @Post('create-meal')
+  async createMeal(@Body() input: CreateMealDto, @Req() request: Request) {
+    const userId = request.user['userId']
+    return this.mealService.create(input, userId)
+  }
 }
