2605cdd84ae15a2660eee0e6c483e6a37126eefe
Abdou <kirakira1614@gmail.com>
2022-12-16 22:16:46 +0100

    Get Cheap Meal

--- src/meal/meal.controller.ts
+++ src/meal/meal.controller.ts
@@ -1,10 +1,11 @@
 import { UpdateMealDto } from './dto/update-meal.dto';
 import { CreateMealDto } from './dto/create-meal.dto';
-import { Body, Controller, Get, Param, Post, Put, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Req } from '@nestjs/common';
 import { MealService } from './meal.service';
 import { Request } from 'express';
 import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Meal } from './schema/meal.schema';
+import { Public } from 'src/auth/public.decorator';
 
 @ApiTags('Meal')
 @Controller('meal')
@@ -28,18 +29,28 @@ export class MealController {
     type: CreateMealDto
   })
   @ApiOperation({ summary: 'Find All Meals' })
+  @Public()
   @Get()
   async findAll(): Promise<Meal[]> {
     return this.mealService.findAll()
   }
 
+  @ApiOperation({ summary: 'Get Top 5 Cheapeast Meals' })
+  @Public()
+  @Get('cheap')
+  async findTopCheapMeal(): Promise<Meal[]> {
+    return this.mealService.findCheapMeal()
+  }
+
   @ApiOperation({ summary: 'Find Meals By ID' })
-  @Get('id')
+  @Public()
+  @Get(':id')
   async findById(@Param('id') id: string) {
     return this.mealService.findByID(id)
   }
 
   @ApiOperation({ summary: 'Find Restaurant By Its ID' })
+  @Public()
   @Get('find-restaurant/:id')
   async findRestaurant(@Param('id') restaurantId: string) {
     return this.mealService.findAllRestaurant(restaurantId)
@@ -51,10 +62,20 @@ export class MealController {
   })
   @ApiOperation({ summary: 'Update Meal' })
   @ApiBearerAuth()
-  @Put('update-meal/:id')
+  @Patch('update-meal/:id')
   async update(@Param('id') id: string, @Body() input: UpdateMealDto, @Req() request: Request) {
     const { userId } = request.user as { userId: string }
     return this.mealService.updateMeal(id, input, userId)
   }
 
+  @ApiOkResponse({
+    description: 'Meal Deleted Successful',
+  })
+  @ApiOperation({ summary: 'Delete Meal' })
+  @ApiBearerAuth()
+  @Delete(':id')
+  async deleteMeal(@Param('id') id: string, @Req() request: Request) {
+    const { userId } = request.user as { userId: string }
+    return this.mealService.deleteMeal(id, userId)
+  }
 }
