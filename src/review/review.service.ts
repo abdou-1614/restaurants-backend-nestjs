7536e275dc5b9f32e0036a7c4870982d3183ddf6
Abdou <kirakira1614@gmail.com>
2022-12-21 23:28:38 +0100

    Create Review Service

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { UserDocument } from './../user/schema/user.schema';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Review, ReviewModel } from './schema/review.schema';
+import { Model } from 'mongoose';
+import { Restaurant, RestaurantDocument } from 'src/restaurant/schema/restaurant.schema';
+import { CreateReviewDto } from './dto/create-review.dto';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+    constructor(
+        @InjectModel(Review.name) private reviewModel: ReviewModel,
+        @InjectModel(Restaurant.name) private restaurantModel: Model<Restaurant>
+    ) {}
+
+    async createReview(input: CreateReviewDto, user: UserDocument['_id'], restaurantId: RestaurantDocument['_id']) {
+        const checkReview = await this.reviewModel.find({user, restaurantId})
+
+         // 1) Check if the user make a review before on that product
+
+         if(checkReview.length !== 0) {
+            throw new BadRequestException('Only One Review')
+         }
+
+         await this.reviewModel.calcAvgRating(restaurantId, this.restaurantModel)
+
+         return this.reviewModel.create({
+            ...input,
+            restaurant: restaurantId,
+            user
+         })
+    }
+}
