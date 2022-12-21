3c28e1d5f5fe4a08a653d99000ff8e56f635ef1f
Abdou <kirakira1614@gmail.com>
2022-12-21 23:29:04 +0100

    Create Review Controller

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Param, Post, Req } from '@nestjs/common';
 import { ReviewService } from './review.service';
+import { Request } from 'express';
 
+@ApiTags('Review')
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+
+  @Post('create-review/:id')
+  async create(@Param('id') restaurantId: string, @Body() input: CreateReviewDto, @Req() request: Request) {
+    const { userId } = request.user as { userId: string }
+    return this.reviewService.createReview(input, userId, restaurantId)
+  }
 }
