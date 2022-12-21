61690d73fe3c36aa0789342d23f3b4a91f454ee6
Abdou <kirakira1614@gmail.com>
2022-12-21 23:28:02 +0100

    Import Mongoose Module

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,8 +1,14 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
+import { ReviewSchema } from './schema/review.schema';
+import { RestaurantSchema } from 'src/restaurant/schema/restaurant.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: 'Review', schema: ReviewSchema}, { name: 'Restaurant', schema: RestaurantSchema }])
+  ],
   controllers: [ReviewController],
   providers: [ReviewService]
 })
