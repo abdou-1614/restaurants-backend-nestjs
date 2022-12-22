6b7f61e3fa8f89ee1b8f75408e3b7d35cedcaf6f
Abdou <kirakira1614@gmail.com>
2022-12-22 23:04:03 +0100

    find review by it's ID

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,8 +1,9 @@
 import { CreateReviewDto } from './dto/create-review.dto';
-import { ApiTags } from '@nestjs/swagger';
-import { Body, Controller, Param, Post, Req } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { Request } from 'express';
+import { Public } from 'src/auth/public.decorator';
 
 @ApiTags('Review')
 @Controller('review')
@@ -10,9 +11,18 @@ export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
 
+  @ApiOperation({ summary: 'Create Review For Restaurant' })
+  @ApiBearerAuth()
   @Post('create-review/:id')
   async create(@Param('id') restaurantId: string, @Body() input: CreateReviewDto, @Req() request: Request) {
     const { userId } = request.user as { userId: string }
     return this.reviewService.createReview(input, userId, restaurantId)
+  }
+
+  @ApiOperation({ summary: 'Find Review By It"s ID' })
+  @Public()
+  @Get('/:reviewId')
+  async findById(@Param('reviewId') reviewId: string) {
+    return this.reviewService.findReviewById(reviewId)
   }
 }
